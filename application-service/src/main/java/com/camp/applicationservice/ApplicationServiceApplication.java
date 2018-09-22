b1fd8bce5de8300de1303ba7f1f2d51f243de0cb
Jakub Homlala <jhomlala@gmail.com>
2018-09-22 21:50:27 +0200

    Added notifications-service, registry, config modules
    Spring Cloud Config, Spring Eureka

--- application-service/src/main/java/com/camp/applicationservice/ApplicationServiceApplication.java
+++ application-service/src/main/java/com/camp/applicationservice/ApplicationServiceApplication.java
@@ -2,8 +2,11 @@ package com.camp.applicationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
 public class ApplicationServiceApplication {
  
 	public static void main(String[] args) {
