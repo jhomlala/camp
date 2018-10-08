ac39e6e2a22a161639236aab490ab83506f1100a
Jakub Homlala <jhomlala@gmail.com>
2018-10-08 21:38:28 +0200

    Added hystrix turbine service and hystrix monitoring

--- application-service/src/main/java/com/camp/applicationservice/ApplicationServiceApplication.java
+++ application-service/src/main/java/com/camp/applicationservice/ApplicationServiceApplication.java
@@ -2,15 +2,18 @@ package com.camp.applicationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class ApplicationServiceApplication {
  
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationServiceApplication.class, args);
 	}
+	
 }
